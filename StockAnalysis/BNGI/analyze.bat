df83ef8f41a2dd58d2447a2c2809b3ff71533200
David McReynolds <dmcreynolds@guidewire.com>
2020-02-14 14:49:26 -0500

    clean up ignored files

--- StockAnalysis/BNGI/analyze.bat
+++
@@ -1,5 +0,0 @@
-set JAVA_HOME=C:\apps\jdk-11.0.3
-set INPUT_HOME=-Dcom.ga.fidelity.trades.home=.\
-set FIDELITY_TRADES_HOME=Z:\fidelity_trades
-set LOG4J_CONF=-Dlog4j.configuration=file:%FIDELITY_TRADES_HOME%\log4j.xml
-%JAVA_HOME%\bin\java %LOG4J_CONF% %INPUT_HOME% -classpath %FIDELITY_TRADES_HOME%\fidelity-trades.jar com.gravanalitical.fidelity.trades.Main
